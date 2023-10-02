b1f3fe5d6dc0141a0c81f0716abefeb5b734bd19
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-02 12:06:40 +0600

    double take 16 digit correctly

--- introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-10_data-types-limitation-c/limitations.c
+++ introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-10_data-types-limitation-c/limitations.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  int a = 1000000000;
-  long long int b = 1000000000000000000;
+  // int a = 1000000000;
+  // long long int b = 1000000000000000000;
 
-  printf("%lld", b);
+  // printf("%lld", b);
+
+  double a = 20.1234567891234;
+  printf("%0.15lf", a);
 
   return 0;
 }

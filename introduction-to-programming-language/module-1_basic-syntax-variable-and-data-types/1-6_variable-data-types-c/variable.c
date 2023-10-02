2b3e9bc6301134437ed9b7f972a8e9b4711d7e03
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-02 10:35:27 +0600

    char variable

--- introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-6_variable-data-types-c/variable.c
+++ introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-6_variable-data-types-c/variable.c
@@ -4,8 +4,11 @@ int main() {
   // int abul = 100, babul = 200;
   // printf("%d %d", abul, babul);
 
-  float num = 2.553;
-  printf("%0.2f", num);
+  // float num = 2.553;
+  // printf("%0.2f", num);
+
+  char name = 'fad';
+  printf("%c", name);
 
   return 0;
 }

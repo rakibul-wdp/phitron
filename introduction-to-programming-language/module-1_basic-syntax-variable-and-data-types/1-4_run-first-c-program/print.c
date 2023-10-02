ac792ad7444ed1c898ffae8bdfcece39187eb488
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-02 10:10:46 +0600

    first c program new like tab percentage

---
+++ introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-4_run-first-c-program/print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello \nSecond h\tello");
+  printf("\\");
+  printf("\n100%%");
+
+  return 0;
+}
+
+// comment
+
+// special characters (ESCAPE)
+// \n - new line
+// \t - tab

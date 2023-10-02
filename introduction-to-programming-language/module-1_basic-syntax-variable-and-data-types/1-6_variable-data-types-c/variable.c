2ec10286f969d64b18d8a8041df8cac7a0753b29
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-02 10:33:08 +0600

    int and float value

---
+++ introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-6_variable-data-types-c/variable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  // int abul = 100, babul = 200;
+  // printf("%d %d", abul, babul);
+
+  float num = 2.553;
+  printf("%0.2f", num);
+
+  return 0;
+}
+
+/*
+format specifier
+%d -> int
+%f -> float
+%c -> char
+*/

3d5c46b8ba4d332cb97abef1eb272306a2c66b4e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-02 11:40:53 +0600

    take direct percentage as a input

--- introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-8_how-to-take-input-c/output.c
+++ introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-8_how-to-take-input-c/output.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
 int main() {
-  int num, num2;
-  char name;
-  float decimal;
+  // int num, num2;
+  // char name;
+  // float decimal;
 
   // scanf("%d %f %c", &num, &decimal, &name);
   // printf("%d %0.2f %c", num, decimal, name);
 
-  int percent, percent2;
-  char p;
+  // int percent, percent2;
 
-  scanf("%d%c %d%c", &percent,&p, &percent2,&p);
-  printf("%c", p);
+  // scanf("%d%% %d%%", &percent, &percent2);
   // printf("%d%% %d%%", percent, percent2);
+
+  char percentageInput[10];
+
+  scanf("%s", percentageInput);
+
+  int percentage = atoi(percentageInput);
+
+  printf("%d%%", percentage);
 
   return 0;
 }

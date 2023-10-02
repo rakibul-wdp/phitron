0f261abe164348822811f543ca624dda21eceed4
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-02 11:24:49 +0600

    take input of several data types

--- introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-8_how-to-take-input-c/output.c
+++ introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-8_how-to-take-input-c/output.c
@@ -2,9 +2,11 @@
 
 int main() {
   int num, num2;
+  char name;
+  float decimal;
 
-  scanf("%d %d", &num, &num2);
-  printf("%d %d", num, num2);
+  scanf("%d %f %c", &num, &decimal, &name);
+  printf("%d %0.2f %c", num, decimal, name);
 
   return 0;
 }

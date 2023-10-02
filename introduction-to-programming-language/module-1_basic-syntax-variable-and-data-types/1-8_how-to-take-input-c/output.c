c625f97a8e403e0041a8bca2f651f1eee99ecbd5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-02 11:34:39 +0600

    take percentage input as a character

--- introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-8_how-to-take-input-c/output.c
+++ introduction-to-programming-language/module-1_basic-syntax-variable-and-data-types/1-8_how-to-take-input-c/output.c
@@ -5,8 +5,21 @@ int main() {
   char name;
   float decimal;
 
-  scanf("%d %f %c", &num, &decimal, &name);
-  printf("%d %0.2f %c", num, decimal, name);
+  // scanf("%d %f %c", &num, &decimal, &name);
+  // printf("%d %0.2f %c", num, decimal, name);
+
+  int percent, percent2;
+  char p;
+
+  scanf("%d%c %d%c", &percent,&p, &percent2,&p);
+  printf("%c", p);
+  // printf("%d%% %d%%", percent, percent2);
 
   return 0;
 }
+
+/*
+two way to take percent input
+1. as a character
+2. as a percentage
+*/

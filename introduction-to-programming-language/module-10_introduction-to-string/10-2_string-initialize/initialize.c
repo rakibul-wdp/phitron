5ea164063c8337cf8532b514752d90c2b2cc3d20
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-16 09:18:12 +0600

    array will take string in one string

---
+++ introduction-to-programming-language/module-10_introduction-to-string/10-2_string-initialize/initialize.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  // int arr[5] = {'a', 'b', 'u', 'l', 'b'};
+  char arr[5] = "Abulb";
+
+  for (int i = 0; i < 5; i++)
+  {
+    printf("%c ", arr[i]);
+  }
+
+  return 0;
+}

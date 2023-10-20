aabeddf43d619ba363a17add8a57467e7c671247
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-21 01:39:00 +0600

    arithmetic operators

---
+++ introduction-to-programming-language/module-2_operators-conditional-statements/2-1_arithmetic-operators/arithmetic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int a = 2, b = 5;
+  int sum = a + b;
+  int sub = b - a;
+  float dvd = b*1.0 / a;
+
+  printf("%0.2f", dvd);
+
+  printf("\n");
+  return 0;
+}

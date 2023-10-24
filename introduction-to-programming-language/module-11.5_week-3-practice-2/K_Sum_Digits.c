97010b0cb16772b1b9945223bbbf120ad61e3cc8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-24 20:36:23 +0600

    module 13 topics

---
+++ introduction-to-programming-language/module-11.5_week-3-practice-2/K_Sum_Digits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+
+  for (int i = 0; i < 1; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+
+  for (int i = 0; i < 1; i++)
+  {
+    printf("%d\n", arr[i]);
+  }
+
+  return 0;
+}
---
+++ introduction-to-programming-language/module-13_nested-loop-recap/13-0_introduction/README.md
@@ -0,0 +1,6 @@
+## Nested Loop Recap
+
+- will see nested loop again
+- will print pattern by using nested loop
+- will solve problem by using nested loop
+- will use sorting by using nested loop

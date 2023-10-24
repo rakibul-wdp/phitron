8783b3149c883c62f7b7a2b80ea361e615b017ab
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-24 07:06:13 +0600

    strcat

--- introduction-to-programming-language/module-11_string-operation-and-frequency-array/11-3_string-concatenation/again_concat.c
+++ introduction-to-programming-language/module-11_string-operation-and-frequency-array/11-3_string-concatenation/again_concat.c
@@ -6,12 +6,7 @@ int main()
   char a[200], b[100];
   scanf("%s %s", a, b);
 
-  int k = strlen(a);
-  for (int i = 0; i <= strlen(b); i++)
-  {
-    a[k] = b[i];
-    k++;
-  }
+  strcat(a, b);
 
   printf("%s %s", a, b);
 

b78e649175b953ff804fecefa0fc83492406f6d4
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-26 03:39:00 +0600

    solved prin it problem

---
+++ introduction-to-programming-language/module-4_assignment-1/assignment-1/print.c
@@ -0,0 +1,30 @@
+/*
+Problem Statement
+Welcome to the "Panta Vat" assignment. In this task you just need to print the following lines as it is.
+
+Hello, world! I am learning C programming language. ^_^
+Programming is fun and challenging. /\/\/\
+I want to give my 100% dedication to learn!	I will succeed one day.
+
+Note: Here you will see 4 spaces in the last line which is a tab, you need to print a tab there.
+
+Input Format
+There is no input
+
+Output Format
+Output the lines.
+
+Sample Output 0
+Hello, world! I am learning C programming language. ^_^
+Programming is fun and challenging. /\/\/\
+I want to give my 100% dedication to learn!    I will succeed one day.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello, world! I am learning C programming language. ^_^\nProgramming is fun and challenging. /\\/\\/\\\nI want to give my 100%% dedication to learn!\tI will succeed one day.");
+
+  return 0;
+}

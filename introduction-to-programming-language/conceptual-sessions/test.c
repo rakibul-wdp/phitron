5cddc979cfe0ba97f55541a47b45d4541530e4c9
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-28 03:39:00 +0600

    conditio conceptual sessions

--- introduction-to-programming-language/conceptual-sessions/test.c
+++ introduction-to-programming-language/conceptual-sessions/test.c
@@ -2,15 +2,14 @@
 
 int main()
 {
-  int a, b;
-
-  printf("Enter num1: ");
-  scanf("%d", &a);
-
-  printf("Enter num2: ");
-  scanf("%d", &b);
-
-  printf("%d %d", a, b);
+  for (int i = 1; i <= 10; i++)
+  {
+    if (i % 2 == 0)
+      continue;
+    if (i == 5)
+      break;
+    printf("Square of %d is %d\n", i, i * i);
+  }
 
   return 0;
 }

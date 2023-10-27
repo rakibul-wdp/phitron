d03bce67f72430e88f5bd64912252cd67552ff40
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-28 02:39:00 +0600

    data type conceptual sessions

---
+++ introduction-to-programming-language/conceptual-sessions/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int a, b;
+
+  printf("Enter num1: ");
+  scanf("%d", &a);
+
+  printf("Enter num2: ");
+  scanf("%d", &b);
+
+  printf("%d %d", a, b);
+
+  return 0;
+}

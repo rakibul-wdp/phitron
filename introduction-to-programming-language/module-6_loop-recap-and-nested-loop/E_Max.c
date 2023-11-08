669300c071a0a66646ea67ed1711fa9fcda8ea50
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-09 01:39:00 +0600

    solve msx problem by myself

---
+++ introduction-to-programming-language/module-6_loop-recap-and-nested-loop/E_Max.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+
+  int a, max = 0;
+  for (int i = i; i <= n; i++)
+  {
+    scanf("%d", &a);
+
+    if (max < a)
+      max = a;
+  }
+  printf("%d\n", max);
+
+  return 0;
+}

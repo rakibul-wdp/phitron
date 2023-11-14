b640e107380ddeea5fb8b4f7a202a6533180b2a2
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-14 09:55:50 +0600

    c simple calculator

---
+++ introduction-to-cpp-for-dsa/module-2.5_week-1-practice-1/C_Simple_Calculator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int m, n;
+  cin >> m >> n;
+
+  cout << m << " + " << n << " = " << m + n << endl;
+  cout << m << " * " << n << " = " << m * n << endl;
+  cout << m << " - " << n << " = " << m - n << endl;
+
+  return 0;
+}

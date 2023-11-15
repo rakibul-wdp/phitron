cf19656b64a6eb1640045ab535bc36f811839af3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-15 10:05:17 +0600

    h sorting

--- introduction-to-cpp-for-dsa/module-2.5_week-1-practice-1/H_Sorting.cpp
+++ introduction-to-cpp-for-dsa/module-2.5_week-1-practice-1/H_Sorting.cpp
@@ -9,11 +9,13 @@ int main()
   for (int i = 0; i < n; i++)
     cin >> arr[i];
 
-  int min = arr[0];
   for (int i = 0; i < n; i++)
   {
-    if (min < arr[i])
-      swap(min, arr[i]);
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      if (arr[j] > arr[j + 1])
+        swap(arr[j], arr[j + 1]);
+    }
   }
 
   for (int i = 0; i < n; i++)

741ba7a7dc8f801336b1a8834959eda27b188980
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-18 20:11:38 +0600

    string class topics

---
+++ introduction-to-cpp-for-dsa/module-05_string-class/5-0_introduction/README.md
@@ -0,0 +1,7 @@
+## String Class
+
+- string
+- built-in function of string
+- how to take input with space
+- how to find word by word
+- iterator
--- introduction-to-cpp-for-dsa/module-3.5_week-1-practice-1/O_Sort_String.cpp
+++ introduction-to-cpp-for-dsa/module-3.5_week-1-practice-1/O_Sort_String.cpp
@@ -9,10 +9,20 @@ int main()
   for (int i = 0; i < n; i++)
     cin >> s[i];
 
-  sort(s, s + n);
-
+  int count[26] = {0};
   for (int i = 0; i < n; i++)
-    cout << s[i];
+  {
+    int value = s[i] - 'a';
+    count[value]++;
+  }
+
+  for (int i = 0; i < 26; i++)
+  {
+    // int value = s[i] - 'a';
+    if (count[i] != 0)
+      printf("%c - %d\n", i + 'a', count[i]);
+    // count[i] = 0;
+  }
 
   return 0;
 }

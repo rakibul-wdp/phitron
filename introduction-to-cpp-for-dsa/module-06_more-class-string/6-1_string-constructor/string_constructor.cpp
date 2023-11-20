ca9763ae1340afd6539049f63cf790935d423813
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-20 09:39:34 +0600

    string constructor

---
+++ introduction-to-cpp-for-dsa/module-06_more-class-string/6-1_string-constructor/string_constructor.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // string s = "hello world";
+
+  // string s("hello world!");
+
+  // string s("Hello world!", 7);
+
+  // string a = "hello world";
+  // string s(a, 4);
+
+  string s(26, 'A');
+
+  cout << s << endl;
+
+  return 0;
+}

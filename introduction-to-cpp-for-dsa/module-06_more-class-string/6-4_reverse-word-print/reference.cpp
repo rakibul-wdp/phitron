dfd5a7db960654b9fb63c2ab3bee748dc800a6b3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-20 10:22:34 +0600

    string reference

---
+++ introduction-to-cpp-for-dsa/module-06_more-class-string/6-4_reverse-word-print/reference.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(string &s)
+{
+  s = "world";
+}
+
+int main()
+{
+  string s = "hello";
+  print(s);
+
+  cout << s << endl;
+
+  return 0;
+}

923668e94bad493b472932624323768015a392be
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-20 11:33:42 +0600

    dynamic obj copy

---
+++ introduction-to-cpp-for-dsa/module-06_more-class-string/6-7_copy-dynamic-obj/dynamic_obj_copy.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Person
+{
+public:
+  string name;
+  int age;
+  Person(string name, int age)
+  {
+    this->name = name;
+    this->age = age;
+  }
+};
+
+int main()
+{
+  Person *abul = new Person("abu babul", 24);
+  Person *babul = new Person("babul cabul", 25);
+
+  // abul = babul;
+
+  // abul->name = babul->name;
+  // abul->age = babul->age;
+
+  *abul = *babul;
+
+  delete babul;
+
+  cout << abul->name << " " << abul->age << endl;
+
+  return 0;
+}

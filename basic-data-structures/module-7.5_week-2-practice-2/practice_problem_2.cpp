0a5ef674453873422773fca0ee229831a6a29b3a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-07 18:55:55 +0600

    practice problem size check

---
+++ basic-data-structures/module-7.5_week-2-practice-2/practice_problem_2.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  Node *next;
+  Node(int val)
+  {
+    this->val = val;
+    this->next = NULL;
+  }
+};
+
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+  Node *newNode = new Node(val);
+  if (head == NULL)
+  {
+    head = newNode;
+    tail = newNode;
+    return;
+  }
+  tail->next = newNode;
+  tail = newNode;
+}
+
+int size(Node *head)
+{
+  Node *tmp = head;
+  int count = 0;
+  while (tmp != NULL)
+  {
+    count++;
+    tmp = tmp->next;
+  }
+  return count;
+}
+
+int main()
+{
+  Node *head1 = NULL;
+  Node *tail1 = NULL;
+
+  Node *head2 = NULL;
+  Node *tail2 = NULL;
+
+  int val1, val2;
+  while (true)
+  {
+    cin >> val1;
+    if (val1 == -1)
+      break;
+    insert_tail(head1, tail1, val1);
+  }
+  while (true)
+  {
+    cin >> val2;
+    if (val2 == -1)
+      break;
+    insert_tail(head2, tail2, val2);
+  }
+
+  if (size(head1) == size(head2))
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+
+  return 0;
+}

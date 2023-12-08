c16c54061d50ecddb45ae3bbec9331a0caf14114
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-09 01:39:00 +0600

    string initialization

---
+++ introduction-to-programming-language/module-10_introduction-to-string/10-2_string-initialize/again_initialize.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  // int a[5] = {5, 4, 3, 2, 1};
+  char a[5] = "Babul";
+
+  // int siz = sizeof(a) / sizeof(char);
+  // printf("%d", siz);
+
+  printf("%s", a);
+
+  return 0;
+}

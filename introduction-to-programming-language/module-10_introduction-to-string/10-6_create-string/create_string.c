f2fc41cc2daeeab5006bd5b47c8be261c75b6dee
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-10 02:39:00 +0600

    again create a new string

---
+++ introduction-to-programming-language/module-10_introduction-to-string/10-6_create-string/create_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char s[1001], t[1001];
+  scanf("%s %s", s, t);
+
+  int len_s = strlen(s);
+  int len_t = strlen(t);
+
+  printf("%d %d\n", len_s, len_t);
+  printf("%s %s\n", s, t);
+
+  return 0;
+}

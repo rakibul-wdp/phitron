a059521cf67c4e81516838c5c92c966d32a0a670
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-11 01:39:00 +0600

    count

---
+++ introduction-to-programming-language/module-10_introduction-to-string/10-8_count/again_count.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char s[1000001];
+  scanf("%s", s);
+
+  int sum = 0;
+  for (int i = 0; i < strlen(s); i++)
+  {
+    sum += s[i] - 48;
+  }
+
+  printf("%d", sum);
+
+  return 0;
+}

6c30cdf85e6d8f4652f39023b9d1a3079b9a4f1f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-12 02:39:00 +0600

    learn pattern two

--- introduction-to-programming-language/module-13_nested-loop-recap/13-2_pattern-2/cpp.cpp
+++ introduction-to-programming-language/module-13_nested-loop-recap/13-2_pattern-2/cpp.cpp
@@ -3,18 +3,7 @@ using namespace std;
 
 int main()
 {
-  int n, k = 1;
-  cin >> n;
-
-  for (int i = 1; i <= n; i++)
-  {
-    for (int j = n; j >= k; j--)
-    {
-      cout << "*";
-    }
-    k++;
-    cout << endl;
-  }
+  int n, s, k = 1;
 
   return 0;
 }
--- introduction-to-programming-language/module-13_nested-loop-recap/13-2_pattern-2/pattern_2.c
+++ introduction-to-programming-language/module-13_nested-loop-recap/13-2_pattern-2/pattern_2.c
@@ -2,8 +2,22 @@
 
 int main()
 {
-  int n, k = 1;
+  int n, s, k = 1;
   scanf("%d", &n);
+
+  s = n - 1;
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= s; j++)
+      printf(" ");
+    for (int j = 1; j <= k; j++)
+      printf("*");
+
+    s--;
+    k += 2;
+    printf("\n");
+  }
 
   return 0;
 }

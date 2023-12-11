e24a663b18f8734919e9c92cc28c593aed1558a4
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-12 03:39:00 +0600

    solve shape two problem with cpp in codeforces

--- introduction-to-programming-language/module-13_nested-loop-recap/13-2_pattern-2/cpp.cpp
+++ introduction-to-programming-language/module-13_nested-loop-recap/13-2_pattern-2/cpp.cpp
@@ -4,6 +4,25 @@ using namespace std;
 int main()
 {
   int n, s, k = 1;
+  cin >> n;
+
+  s = n - 1;
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= s; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= k; j++)
+    {
+      cout << "*";
+    }
+
+    s--;
+    k += 2;
+    cout << endl;
+  }
 
   return 0;
 }

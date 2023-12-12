27a8ed0866da059097e23eeee18f9923fd52dffd
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-13 03:39:00 +0600

    sum of two values equal x with video

--- introduction-to-programming-language/module-13_nested-loop-recap/13-6_sum-of-two-value-equal-x/sum_of_two.c
+++ introduction-to-programming-language/module-13_nested-loop-recap/13-6_sum-of-two-value-equal-x/sum_of_two.c
@@ -7,14 +7,13 @@ int main()
   int arr[n];
   for (int i = 0; i < n; i++)
     scanf("%d", &arr[i]);
-
   int x;
   scanf("%d", &x);
 
   int a, b;
   for (int i = 0; i < n - 1; i++)
   {
-    for (int j = 1; j < n; j++)
+    for (int j = i + 1; j < n; j++)
     {
       if (arr[i] + arr[j] == x)
       {

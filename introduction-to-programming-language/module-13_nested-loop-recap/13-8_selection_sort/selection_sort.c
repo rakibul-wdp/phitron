ebc0a5c2f53eadcb0fa88436a85939be78d8f657
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-14 02:39:00 +0600

    selection sort

--- introduction-to-programming-language/module-13_nested-loop-recap/13-8_selection_sort/selection_sort.c
+++ introduction-to-programming-language/module-13_nested-loop-recap/13-8_selection_sort/selection_sort.c
@@ -8,6 +8,21 @@ int main()
   for (int i = 0; i < n; i++)
     scanf("%d", &arr[i]);
 
+  // ascending
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = i + 1; j < n; j++)
+    {
+      if (arr[i] > arr[j])
+      {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  // descending
   for (int i = 0; i < n - 1; i++)
   {
     for (int j = i + 1; j < n; j++)

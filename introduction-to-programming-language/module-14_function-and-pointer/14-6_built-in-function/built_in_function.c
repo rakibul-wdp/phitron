be569140880ddab3d65694c2e5abf2ef4b8fbf2b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-16 03:39:00 +0600

    build in math function

--- introduction-to-programming-language/module-14_function-and-pointer/14-6_built-in-function/built_in_function.c
+++ introduction-to-programming-language/module-14_function-and-pointer/14-6_built-in-function/built_in_function.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -11,6 +12,14 @@ int main()
 
   printf("%f\n", ceil(1.0001));
   printf("%f\n", floor(1.9999));
+
+  printf("%f\n", round(3.5));
+
+  printf("%f\n", sqrt(24));
+
+  printf("%f\n", pow(3.5, 3));
+
+  printf("%d\n", abs(-3.99));
 
   return 0;
 }

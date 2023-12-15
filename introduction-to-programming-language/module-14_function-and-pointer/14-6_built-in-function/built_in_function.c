b08d72a34a6f1140c4ea62bf8dce5fd737d669a6
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-16 02:39:00 +0600

    ceil and floor

---
+++ introduction-to-programming-language/module-14_function-and-pointer/14-6_built-in-function/built_in_function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  // float x;
+  // scanf("%f", &x);
+
+  // int res = ceil(x);
+  // printf("%d", res);
+
+  printf("%f\n", ceil(1.0001));
+  printf("%f\n", floor(1.9999));
+
+  return 0;
+}

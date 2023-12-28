cec25cbd440bd814da6ff3cd7431de78d1fb2bf6
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-29 01:39:00 +0600

    solve coutn 3 from mid term exam

--- introduction-to-programming-language/module-12_mid-term-exam/count_me_3.c
+++ introduction-to-programming-language/module-12_mid-term-exam/count_me_3.c
@@ -10,10 +10,17 @@ int main()
   {
     scanf("%s", s);
 
+    int capital = 0, small = 0, number = 0;
     for (int i = 0; i < strlen(s); i++)
     {
-      printf("%d", s[i]);
+      if (s[i] >= 65 && s[i] <= 90)
+        capital++;
+      else if (s[i] >= 97 && s[i] <= 122)
+        small++;
+      else if (s[i] >= 48 && s[i] <= 57)
+        number++;
     }
+    printf("%d %d %d\n", capital, small, number);
   }
 
   return 0;

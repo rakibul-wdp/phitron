5426f15a719b15c7d396f607714eec98dc8f4c6f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-10 02:39:00 +0600

    switch case

---
+++ introduction-to-cpp-for-dsa/module-01_basic-cpp/1-6_switch-case/switch_case.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  switch (n)
+  {
+  case 1:
+    cout << "one" << endl;
+    break;
+  case 2:
+    cout << "two" << endl;
+    break;
+  case 3:
+    cout << "three" << endl;
+    break;
+
+  default:
+    cout << "nothing match" << endl;
+  }
+
+  return 0;
+}

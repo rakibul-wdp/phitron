fb17da0a939c887518349b2dcd0f2d6b7c4b52af
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-11 03:39:00 +0600

    min max with multiple value

--- introduction-to-cpp-for-dsa/module-01_basic-cpp/1-7_min-max-swap/min_max_swap.cpp
+++ introduction-to-cpp-for-dsa/module-01_basic-cpp/1-7_min-max-swap/min_max_swap.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-  int a, b;
-  cin >> a >> b;
+  int a, b, c, d, e;
+  cin >> a >> b >> c >> d >> e;
 
-  int res = min(a, b);
-  int res2 = max(a, b);
+  int res = min({a, b, c, d, e});
+  int res2 = max({a, b, c, d, e});
 
-  cout << res << res2 << endl;
+  cout << res << " " << res2 << endl;
 
   return 0;
 }

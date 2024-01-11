02b7854f9f9c274f1ea0deef12733ca08b5efd69
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-12 02:39:00 +0600

    string

---
+++ introduction-to-cpp-for-dsa/module-01_basic-cpp/1-8_string/string.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  char s[100];
+  int a;
+  cin >> a;
+
+  getchar();
+
+  cin.getline(s, 100);
+
+  cout << a << endl;
+  cout << s << endl;
+
+  return 0;
+}

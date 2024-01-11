8b0ba63269c78fa974ede595eec09b50cdb2dc74
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-12 01:39:00 +0600

    manually swap and swap function

---
+++ introduction-to-cpp-for-dsa/module-01_basic-cpp/1-7_min-max-swap/swap.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void my_swap(int *a, int *b)
+{
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+
+  swap(a, b);
+  cout << a << " " << b << endl;
+
+  return 0;
+}

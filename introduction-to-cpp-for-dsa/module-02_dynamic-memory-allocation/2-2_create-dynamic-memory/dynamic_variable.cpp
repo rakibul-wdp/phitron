e7a7d35c30469a1cccdc50782f975b91c4b3d863
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-13 03:39:00 +0600

    dynamic memory with function

--- introduction-to-cpp-for-dsa/module-02_dynamic-memory-allocation/2-2_create-dynamic-memory/dynamic_variable.cpp
+++ introduction-to-cpp-for-dsa/module-02_dynamic-memory-allocation/2-2_create-dynamic-memory/dynamic_variable.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int *fun()
+{
+  int *a = new int;
+  cout << "fun: " << a << endl;
+  *a = 100;
+  return a;
+}
+
 int main()
 {
+  int *p = fun();
+  cout << "main: " << *p << endl;
+
   int *a = new int;
   *a = 10;
   cout << *a << endl;

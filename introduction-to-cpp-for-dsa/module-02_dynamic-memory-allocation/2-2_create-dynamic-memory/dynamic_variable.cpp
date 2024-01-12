954c2e7891a21131299a8db6d4be628ed9ab98e5
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-13 02:39:00 +0600

    dynamic variable

---
+++ introduction-to-cpp-for-dsa/module-02_dynamic-memory-allocation/2-2_create-dynamic-memory/dynamic_variable.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int *a = new int;
+  *a = 10;
+  cout << *a << endl;
+
+  float *f = new float;
+  *f = 1.587;
+  cout << *f << endl;
+
+  return 0;
+}

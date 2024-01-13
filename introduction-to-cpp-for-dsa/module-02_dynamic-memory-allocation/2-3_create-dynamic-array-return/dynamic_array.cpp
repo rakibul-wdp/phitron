184ef7cf0dde365f74d83fe9f6a6b12027e83b9f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-14 01:39:00 +0600

    dynamic array

---
+++ introduction-to-cpp-for-dsa/module-02_dynamic-memory-allocation/2-3_create-dynamic-array-return/dynamic_array.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  int *a = new int[5];
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  for (int i = 0; i < n; i++)
+    cout << a[i] << endl;
+
+  return 0;
+}

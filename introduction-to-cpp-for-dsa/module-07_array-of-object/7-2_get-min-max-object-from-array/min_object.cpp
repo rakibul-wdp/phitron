2e8ba30625069473fdffd5efa449e97f80780b89
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-17 01:39:00 +0600

    get just min marks

---
+++ introduction-to-cpp-for-dsa/module-07_array-of-object/7-2_get-min-max-object-from-array/min_object.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+  string name;
+  int roll;
+  int marks;
+};
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  Student arr[n];
+  for (int i = 0; i < n; i++)
+    cin >> arr[i].name >> arr[i].roll >> arr[i].marks;
+
+  int mn = INT_MAX;
+  for (int i = 0; i < n; i++)
+  {
+    if (arr[i].marks < mn)
+      mn = arr[i].marks;
+  }
+
+  cout << mn << endl;
+
+  return 0;
+}

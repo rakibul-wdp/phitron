c8d0a2c06747dbaee62b539d2e013f7eef7d6b7f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-18 03:39:00 +0600

    sort with sort function

---
+++ introduction-to-cpp-for-dsa/module-07_array-of-object/7-5_sort-usign-sort-fun/sort_with_sort_fun.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+  string name;
+  int roll;
+  int marks;
+};
+
+bool cmp(Student a, Student b)
+{
+  // if (a.marks >= b.marks)
+  //   return true;
+  // else
+  //   return false;
+  return a.marks >= b.marks;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  Student arr[n];
+  for (int i = 0; i < n; i++)
+    cin >> arr[i].name >> arr[i].roll >> arr[i].marks;
+
+  sort(arr, arr + n, cmp);
+
+  for (int i = 0; i < n; i++)
+    cout
+        << arr[i].name << " " << arr[i].roll << " " << arr[i].marks << endl;
+
+  return 0;
+}

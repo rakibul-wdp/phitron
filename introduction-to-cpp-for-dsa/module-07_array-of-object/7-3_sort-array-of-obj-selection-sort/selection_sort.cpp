85c2a61efa6dc6e5ceef2c3d478beac1c62f989e
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-18 01:39:00 +0600

    sort array of objects based on roll marks assending and desending

---
+++ introduction-to-cpp-for-dsa/module-07_array-of-object/7-3_sort-array-of-obj-selection-sort/selection_sort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+  string name;
+  int roll;
+  int marks;
+};
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  Student arr[n];
+  for (int i = 0; i < n; i++)
+    cin >> arr[i].name >> arr[i].roll >> arr[i].marks;
+
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = i + 1; j < n; j++)
+    {
+      if (arr[i].roll < arr[j].roll)
+        swap(arr[i], arr[j]);
+    }
+  }
+
+  for (int i = 0; i < n; i++)
+    cout << arr[i].name << " " << arr[i].roll << " " << arr[i].marks << endl;
+
+  return 0;
+}

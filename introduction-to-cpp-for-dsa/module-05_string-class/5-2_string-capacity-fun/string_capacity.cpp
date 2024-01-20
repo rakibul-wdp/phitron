a6404092aa4c77abbda04303d5d49f937fd45bdc
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-21 02:39:00 +0600

    string clear with clear fun

--- introduction-to-cpp-for-dsa/module-05_string-class/5-2_string-capacity-fun/string_capacity.cpp
+++ introduction-to-cpp-for-dsa/module-05_string-class/5-2_string-capacity-fun/string_capacity.cpp
@@ -7,11 +7,17 @@ int main()
 
   // cout << s.size() << endl;
   // cout << s.max_size() << endl;
-  string a = "abul babul cabul dabul";
-  cout << a.capacity() << endl;
-  a = "hello from the other start i am leaddy on my ohesty";
-  cout << a.capacity() << endl;
-  cout << a.size() << endl;
+  // string a = "abul babul cabul dabul";
+  // cout << a.capacity() << endl;
+  // a = "hello from the other start i am leaddy on my ohesty";
+  // cout << a.capacity() << endl;
+  // cout << a.size() << endl;
+
+  string s = "hello";
+  cout << s << endl;
+  s.clear();
+  cout << s << endl;
+  cout << s.size() << endl;
 
   return 0;
 }

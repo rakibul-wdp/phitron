c12f81382a9d7a755d6f0cc4d700f6c497cd7571
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-21 01:39:00 +0600

    capacity

---
+++ introduction-to-cpp-for-dsa/module-05_string-class/5-2_string-capacity-fun/string_capacity.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // string s = "hello world";
+
+  // cout << s.size() << endl;
+  // cout << s.max_size() << endl;
+  string a = "abul babul cabul dabul";
+  cout << a.capacity() << endl;
+  a = "hello from the other start i am leaddy on my ohesty";
+  cout << a.capacity() << endl;
+  cout << a.size() << endl;
+
+  return 0;
+}

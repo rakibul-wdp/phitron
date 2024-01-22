898398617638a74cd24e129880cf63760001a707
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-23 01:39:00 +0600

    string concatinate with manually and append function

---
+++ introduction-to-cpp-for-dsa/module-05_string-class/5-4_string-modifiers/string_modifiers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string a = "hello";
+  string b = "world";
+  // a += b;
+  a.append(b);
+  cout << a << endl
+       << b << endl;
+
+  return 0;
+}

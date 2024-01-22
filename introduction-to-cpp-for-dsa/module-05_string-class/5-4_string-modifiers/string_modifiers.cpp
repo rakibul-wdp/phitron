3bc2751fbef6d3e69b9e155e3bd28eebda2f3991
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-23 02:39:00 +0600

    string add and remove with pop back and push back

--- introduction-to-cpp-for-dsa/module-05_string-class/5-4_string-modifiers/string_modifiers.cpp
+++ introduction-to-cpp-for-dsa/module-05_string-class/5-4_string-modifiers/string_modifiers.cpp
@@ -6,9 +6,17 @@ int main()
   string a = "hello";
   string b = "world";
   // a += b;
-  a.append(b);
-  cout << a << endl
-       << b << endl;
+  // a.append(b);
+  // cout << a << endl
+  //      << b << endl;
+
+  // a[5] = 'A'; // didn't works
+  // a = "helloA";
+  // a = a + 'A';
+  a.push_back('A');
+  a.pop_back();
+  a.pop_back();
+  cout << a << endl;
 
   return 0;
 }

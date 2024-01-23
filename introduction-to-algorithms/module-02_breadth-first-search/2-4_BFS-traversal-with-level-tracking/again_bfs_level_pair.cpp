c0694342a3f6699a1c4ec1b5d9291aacedf9d8ac
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-23 17:29:47 +0600

    again bfs level pair

---
+++ introduction-to-algorithms/module-02_breadth-first-search/2-4_BFS-traversal-with-level-tracking/again_bfs_level_pair.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v[1005];
+bool vis[1005];
+void bfs(int src, int des)
+{
+  queue<pair<int, int>> q;
+  q.push({src, 0});
+  vis[src] = true;
+  bool found = false;
+  while (!q.empty())
+  {
+    pair<int, int> p = q.front();
+    q.pop();
+    int par = p.first;
+    int level = p.second;
+    // cout << par << endl;
+    if (par == des)
+    {
+      cout << level << endl;
+      found = true;
+    }
+    for (int child : v[par])
+    {
+      if (!vis[child])
+      {
+        q.push({child, level + 1});
+        vis[child] = true;
+      }
+    }
+  }
+  if (!found)
+    cout << -1 << endl;
+}
+
+int main()
+{
+  int n, e;
+  cin >> n >> e;
+  while (e--)
+  {
+    int a, b;
+    cin >> a >> b;
+    v[a].push_back(b);
+    v[b].push_back(a);
+  }
+
+  int src;
+  cin >> src;
+  memset(vis, false, sizeof(vis));
+  bfs(src, 8);
+
+  return 0;
+}
+
+/*
+5 6
+0 1
+1 2
+0 4
+1 3
+2 0
+3 4
+0
+*/

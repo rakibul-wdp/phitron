5c3f8519e02e2769303ed34f2a1477449b56248f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-23 18:58:48 +0600

    again path print

---
+++ introduction-to-algorithms/module-02_breadth-first-search/2-7_path-printing-using-bfs/again_path_printing.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v[1005];
+bool vis[1005];
+int level[1005];
+int parent[1005];
+void bfs(int src)
+{
+  queue<int> q;
+  q.push(src);
+  vis[src] = true;
+  level[src] = 0;
+  while (!q.empty())
+  {
+    int par = q.front();
+    q.pop();
+    // cout << par << endl;
+    for (int child : v[par])
+    {
+      if (!vis[child])
+      {
+        q.push(child);
+        vis[child] = true;
+        level[child] = level[par] + 1;
+        parent[child] = par;
+      }
+    }
+  }
+}
+
+int main()
+{
+  int n, e;
+  cin >> n >> e;
+  while (e--)
+  {
+    int a, b;
+    cin >> a >> b;
+    v[a].push_back(b);
+    v[b].push_back(a);
+  }
+
+  int src, des;
+  cin >> src >> des;
+  memset(vis, false, sizeof(vis));
+  memset(level, -1, sizeof(level));
+  memset(parent, -1, sizeof(parent));
+  bfs(src);
+
+  int x = des;
+  vector<int> path;
+  while (x != -1)
+  {
+    path.push_back(x);
+    x = parent[x];
+  }
+
+  reverse(path.begin(), path.end());
+
+  for (int val : path)
+    cout << val << " ";
+
+  return 0;
+}
+
+/*
+6 7
+0 1
+1 2
+1 3
+2 4
+2 5
+4 5
+4 3
+0
+5
+*/

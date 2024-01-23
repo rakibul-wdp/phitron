3dabed11e5714d9cb1afb26742ccf002e97199a4
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-24 03:39:00 +0600

    string input with space cin igonore or getchar for ignore inter

---
+++ introduction-to-cpp-for-dsa/module-05_string-class/5-7_string-input-with-spaces/string_input_space.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  string s;
+  // cin.ignore();
+  getchar();
+  getline(cin, s);
+
+  cout << s << endl;
+
+  return 0;
+}

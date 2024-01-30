eb2886f0b15e589fc86612aa213906b39c8a9c00
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-31 03:39:00 +0600

    vector element access functionality

---
+++ basic-data-structures/module-02_stl-vector/2-5_vector-access-and-iterators/vector_access.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  vector<int> v = {7, 8, 9};
+
+  cout << v[v.size() - 1];
+  cout << v.back();
+  cout << v[0];
+  cout << v.front();
+
+  return 0;
+}

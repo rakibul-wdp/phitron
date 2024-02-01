74e4f231d83c52ee7a08a5cb4b28177217ea4fb3
rakibul-wdp <rakibul.wdp@gmail.com>
2024-02-02 03:39:00 +0600

    big o of log n time complexity

--- basic-data-structures/module-01_time-and-space-complexity/1-3_O(N)-tc/O_N.cpp
+++ basic-data-structures/module-01_time-and-space-complexity/1-3_O(N)-tc/O_N.cpp
@@ -6,11 +6,11 @@ int main()
   int n;
   cin >> n;
   int arr[n];
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++) // O(N)
     cin >> arr[i];
 
   int sum = 0;
-  for (int i = 0; i < n; i += 2)
+  for (int i = 0; i < n; i += 2) // O(N)
     sum += arr[i];
 
   cout << sum << endl;

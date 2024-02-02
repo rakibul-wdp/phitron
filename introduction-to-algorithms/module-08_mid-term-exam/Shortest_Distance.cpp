d38a380b6cfaa616ec9c2a7660750ca746984c2d
rakibul-wdp <rakibul.wdp@gmail.com>
2024-02-02 17:55:13 +0600

    shortest distance

---
+++ introduction-to-algorithms/module-08_mid-term-exam/Shortest_Distance.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+  ll n, e;
+  cin >> n >> e;
+  ll adj[n + 1][n + 1];
+
+  for (int i = 0; i <= n; i++)
+  {
+    for (int j = 0; j <= n; j++)
+    {
+      adj[i][j] = 1e18;
+      if (i == j)
+        adj[i][j] = 0;
+    }
+  }
+
+  while (e--)
+  {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    adj[a][b] = min(adj[a][b], c);
+  }
+
+  for (int k = 0; k <= n; k++)
+  {
+    for (int i = 0; i <= n; i++)
+    {
+      for (int j = 0; j <= n; j++)
+      {
+        if (adj[i][k] != 1e18 && adj[k][j] != 1e18 &&
+            adj[i][k] + adj[k][j] < adj[i][j])
+          adj[i][j] = adj[i][k] + adj[k][j];
+      }
+    }
+  }
+
+  int q;
+  cin >> q;
+  while (q--)
+  {
+    int x, y;
+    cin >> x >> y;
+
+    if (adj[x][y] == 1e18)
+      cout << "-1" << endl;
+    else
+      cout << adj[x][y] << endl;
+  }
+
+  return 0;
+}
+
+/*
+4 6
+3 2 8
+2 1 5
+1 0 2
+3 0 20
+0 1 3
+1 2 2
+*/

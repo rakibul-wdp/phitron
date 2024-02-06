df033b512f03c6d393606c5927ce5a071c98aab7
rakibul-wdp <rakibul.wdp@gmail.com>
2024-02-07 01:39:00 +0600

    sorted

---
+++ basic-data-structures/module-3.5_week-1-practice-2/Sorted.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+      cin >> v[i];
+
+    bool flag = true;
+    for (int i = 0; i < n - 1; i++)
+    {
+      if (v[i] > v[i + 1])
+      {
+        flag = false;
+        break;
+      }
+    }
+
+    if (flag)
+      cout << "YES" << endl;
+    else
+      cout << "NO" << endl;
+  }
+
+  return 0;
+}

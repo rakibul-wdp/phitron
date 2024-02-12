7539759bd695a37ad26b23a6610529ef5210c5d9
rakibul-wdp <rakibul.wdp@gmail.com>
2024-02-13 03:39:00 +0600

    insert singly linked list at tail

---
+++ basic-data-structures/module-07_singly-linked-list-recap/7-3_insert_at_head_tail/insert_tail.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  Node *next;
+  Node(int val)
+  {
+    this->val = val;
+    this->next = NULL;
+  }
+};
+
+void print_linked_list(Node *head)
+{
+  Node *tmp = head;
+  while (tmp != NULL)
+  {
+    cout << tmp->val << " ";
+    tmp = tmp->next;
+  }
+  cout << endl;
+}
+
+int size(Node *head)
+{
+  Node *tmp = head;
+  int count = 0;
+  while (tmp != NULL)
+  {
+    count++;
+    tmp = tmp->next;
+  }
+  return count;
+}
+
+void insert(Node *head, int pos, int val)
+{
+  Node *newNode = new Node(val);
+  Node *tmp = head;
+  for (int i = 1; i <= pos - 1; i++)
+    tmp = tmp->next;
+  // tmp = pos - 1
+  newNode->next = tmp->next;
+  tmp->next = newNode;
+}
+
+void insert_head(Node *&head, int val)
+{
+  Node *newNode = new Node(val);
+  newNode->next = head;
+  head = newNode;
+}
+
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+  Node *newNode = new Node(val);
+  if (head == NULL)
+  {
+    head = newNode;
+    tail = newNode;
+    return;
+  }
+  tail->next = newNode;
+  tail = newNode;
+}
+
+int main()
+{
+  Node *head = new Node(10);
+  Node *a = new Node(20);
+  Node *b = new Node(30);
+  Node *c = new Node(40);
+  Node *d = new Node(50);
+  Node *tail = d;
+
+  head->next = a;
+  a->next = b;
+  b->next = c;
+  c->next = d;
+
+  int pos, val;
+  cin >> pos >> val;
+  if (pos > size(head))
+    cout << "invalid index" << endl;
+  else if (pos == 0)
+    insert_head(head, val);
+  else if (pos == size(head))
+    insert_tail(head, tail, val);
+  else
+    insert(head, pos, val);
+
+  print_linked_list(head);
+
+  return 0;
+}

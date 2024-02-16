475cdf5187b6d554a659e7a703be0b23fd4a3d84
rakibul-wdp <rakibul.wdp@gmail.com>
2024-02-17 02:39:00 +0600

    detect cycle in singly linked list

---
+++ basic-data-structures/module-10_stl-list-and-cycle-detection/10-8_detect-cycle-singly-linked-list/detect_cycly.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  Node *next;
+  Node(int val)
+  {
+    this->val = val;
+    this->next = NULL;
+  }
+};
+
+int main()
+{
+  Node *head = new Node(10);
+  Node *a = new Node(20);
+  Node *b = new Node(30);
+
+  head->next = a;
+  a->next = b;
+
+  Node *slow = head;
+  Node *fast = head;
+
+  bool flag = false;
+  while (fast != NULL && fast->next != NULL)
+  {
+    slow = slow->next;
+    fast = fast->next->next;
+    if (fast == slow)
+    {
+      flag = true;
+      break;
+    }
+  }
+
+  if (flag)
+    cout << "cycle detected" << endl;
+  else
+    cout << "not detected" << endl;
+
+  return 0;
+}

47b98c30ca47cd9c0526390b8a9954a1966f6719
rakibul-wdp <rakibul.wdp@gmail.com>
2024-02-16 19:32:46 +0600

    maze

--- introduction-to-algorithms/module-04_assignment-1/Can_Go?.cpp
+++ introduction-to-algorithms/module-04_assignment-1/Can_Go?.cpp
@@ -76,3 +76,88 @@ int main()
 
   return 0;
 }
+
+/*
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// char arr[1000][1000];
+// bool vis[1000][1000];
+// int dis[1000][1000];
+
+// vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+// int n, m;
+
+// bool valid(int i, int j)
+// {
+//   if (i < 0 || i >= n || j < 0 || j >= m)
+//     return false;
+//   return true;
+// }
+
+// void bfs(int si, int sj)
+// {
+//   queue<pair<int, int>> q;
+//   q.push({si, sj});
+//   vis[si][sj] = true;
+//   dis[si][sj] = 0;
+
+//   while (!q.empty())
+//   {
+//     pair<int, int> par = q.front();
+//     int a = par.first, b = par.second;
+//     q.pop();
+//     for (int i = 0; i < 4; i++)
+//     {
+//       int ci = a + d[i].first;
+//       int cj = b + d[i].second;
+//       if (valid(ci, cj) && !vis[ci][cj] && (arr[ci][cj] == '.' || arr[ci][cj] == 'R' || arr[ci][cj] == 'D'))
+//       {
+//         q.push({ci, cj});
+//         vis[ci][cj] = true;
+//         dis[ci][cj] = dis[a][b] + 1;
+//       }
+//     }
+//   }
+// }
+
+// int main()
+// {
+//   cin >> n >> m;
+//   int si, sj, ei, ej;
+
+//   for (int i = 0; i < n; i++)
+//   {
+//     for (int j = 0; j < m; j++)
+//     {
+//       cin >> arr[i][j];
+//       if (arr[i][j] == 'R')
+//       {
+//         si = i;
+//         sj = j;
+//       }
+//       else if (arr[i][j] == 'D')
+//       {
+//         ei = i;
+//         ej = j;
+//       }
+//     }
+//   }
+
+//   memset(vis, false, sizeof(vis));
+//   memset(dis, -1, sizeof(dis));
+//   bfs(si, sj);
+
+//   for (int i = 0; i < n; i++)
+//   {
+//     for (int j = 0; j < m; j++)
+//     {
+//       cout << arr[i][j];
+//     }
+//     cout << endl;
+//   }
+
+//   return 0;
+// }
+*/
---
+++ introduction-to-algorithms/module-16_assignment-2/Maze.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char arr[1000][1000];
+bool vis[1000][1000];
+int dis[1000][1000];
+
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+int n, m;
+
+bool valid(int i, int j)
+{
+  if (i < 0 || i >= n || j < 0 || j >= m)
+    return false;
+  return true;
+}
+
+void bfs(int si, int sj)
+{
+  queue<pair<int, int>> q;
+  q.push({si, sj});
+  vis[si][sj] = true;
+  dis[si][sj] = 0;
+
+  while (!q.empty())
+  {
+    pair<int, int> par = q.front();
+    int a = par.first, b = par.second;
+    q.pop();
+    for (int i = 0; i < 4; i++)
+    {
+      int ci = a + d[i].first;
+      int cj = b + d[i].second;
+      if (valid(ci, cj) && !vis[ci][cj] && (arr[ci][cj] == '.' || arr[ci][cj] == 'R' || arr[ci][cj] == 'D'))
+      {
+        q.push({ci, cj});
+        vis[ci][cj] = true;
+        dis[ci][cj] = dis[a][b] + 1;
+      }
+    }
+  }
+}
+
+int main()
+{
+  cin >> n >> m;
+  int si, sj, ei, ej;
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cin >> arr[i][j];
+      if (arr[i][j] == 'R')
+      {
+        si = i;
+        sj = j;
+      }
+      else if (arr[i][j] == 'D')
+      {
+        ei = i;
+        ej = j;
+      }
+    }
+  }
+
+  memset(vis, false, sizeof(vis));
+  memset(dis, -1, sizeof(dis));
+  bfs(si, sj);
+
+  int ci = ei, cj = ej;
+  while (ci != si || cj != sj)
+  {
+    if (arr[ci][cj] == '.')
+      arr[ci][cj] = 'X';
+    bool found = false;
+    for (int i = 0; i < 4; i++)
+    {
+      int pi = ci + d[i].first;
+      int pj = cj + d[i].second;
+      if (valid(pi, pj) && dis[pi][pj] == dis[ci][cj] - 1)
+      {
+        ci = pi;
+        cj = pj;
+        found = true;
+        break;
+      }
+    }
+    if (!found)
+      break;
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cout << arr[i][j];
+    }
+    cout << endl;
+  }
+
+  return 0;
+}

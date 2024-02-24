2de24c464e51e52a5952e8d2a858b4792254e681
rakibul-wdp <rakibul.wdp@gmail.com>
2024-02-24 08:40:17 +0600

    changes

---
+++ introduction-to-algorithms/module-19_0-1-knapsack-variations/count_zero_subset_sum.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool dp[1005][1005];
+
+int subset_sum(int n, int arr[], int s)
+{
+  if (n == 0)
+  {
+    if (s == 0)
+      return 1;
+    else
+      return 0;
+  }
+
+  dp[0][0] = true;
+
+  if (arr[n - 1] <= s)
+  {
+    int op1 = subset_sum(n - 1, arr, s - arr[n - 1]);
+    int op2 = subset_sum(n - 1, arr, s);
+    return dp[n][s] = op1 + op2;
+  }
+  else
+    return dp[n][s] = subset_sum(n - 1, arr, s);
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+  int s;
+  cin >> s;
+
+  for (int i = 0; i <= n; i++)
+  {
+    for (int j = 0; j <= s; j++)
+      dp[i][j] = -1;
+  }
+
+  cout << subset_sum(n, arr, s) << endl;
+
+  return 0;
+}
+
+/*
+4
+1 2 4 6
+7
+*/

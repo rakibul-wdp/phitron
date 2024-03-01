db32679fb138361c17506a1f0ea930744698fbc6
rakibul-wdp <rakibul.wdp@gmail.com>
2024-03-02 03:39:00 +0600

    reverse stack using recursion

---
+++ basic-data-structures/module-15_problem-solving-using-stack-&-queue/reverse_stack_using_recursion.cpp
@@ -0,0 +1,22 @@
+void reverseStack(stack<int> &s)
+{
+  // Write your code here
+  if (s.empty())
+    return;
+  int x = s.top();
+  s.pop();
+  reverseStack(s);
+  // reverse is done
+  stack<int> cp;
+  while (!s.empty())
+  {
+    cp.push(s.top());
+    s.pop();
+  }
+  cp.push(x);
+  while (!cp.empty())
+  {
+    s.push(cp.top());
+    cp.pop();
+  }
+}

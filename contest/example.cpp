f8e440b9c3762a6f175d54e45d191471797f8bb2
rakibul-wdp <rakibul.wdp@gmail.com>
2024-03-08 21:38:00 +0600

    MIN Adjacent SUM

--- contest/example.cpp
+++ contest/example.cpp
@@ -3,28 +3,19 @@ using namespace std;
 
 int main()
 {
-  int t;
-  cin >> t;
-  while (t--)
-  {
-    int n;
-    cin >> n;
-    char s[n];
-    for (int i = 0; i < n; i++)
-      cin >> s[i];
+  int n;
+  cin >> n;
+  int A[n];
+  for (int i = 0; i < n; i++)
+    cin >> A[i];
 
-    int count_01 = 0, count_10 = 0;
+  sort(A, A + n);
 
-    for (int i = 0; i < n; i++)
-    {
-      if (s[i] == '0' && s[i + 1] == '1')
-        count_01++;
-      else if (s[i] == '1' && s[i + 1] == '0')
-        count_10++;
-    }
+  int sum = 0;
+  for (int i = 0; i <= 1; i++)
+    sum += A[i];
 
-    cout << count_01 + count_10 << endl;
-  }
+  cout << sum << endl;
 
   return 0;
 }

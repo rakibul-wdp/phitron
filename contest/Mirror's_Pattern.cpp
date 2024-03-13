60a8c32f66d3a9870daa33ecaac87b7547f6e6a0
rakibul-wdp <rakibul.wdp@gmail.com>
2024-03-14 04:39:00 +0600

    mirrors pattern

--- contest/Mirror's_Pattern.cpp
+++ contest/Mirror's_Pattern.cpp
@@ -7,10 +7,19 @@ int main()
   cin >> n;
 
   for (int i = 1; i <= n; i++)
+    cout << i;
+  cout << endl;
+  if (n == 1)
+    return 0;
+  for (int i = 2; i <= n - 1; i++)
   {
     cout << i;
-    cout << i + 1 << endl;
+    for (int j = 1; j <= n - 2; j++)
+      cout << " ";
+    cout << n - i + 1 << endl;
   }
+  for (int i = n; i >= 1; i--)
+    cout << i;
 
   return 0;
 }

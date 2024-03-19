2027ae80ad894f233559086ecee0f8299a3930db
rakibul-wdp <rakibul.wdp@gmail.com>
2024-03-20 02:39:00 +0600

    again max height

---
+++ basic-data-structures/module-18_operations-on-binary-tree/18-7_get-max-height-binary-tree/get_max_height.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  Node *left;
+  Node *right;
+  Node(int val)
+  {
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+Node *input_tree()
+{
+  int val;
+  cin >> val;
+  Node *root;
+  if (val == -1)
+    root = NULL;
+  else
+    root = new Node(val);
+  queue<Node *> q;
+  if (root)
+    q.push(root);
+  while (!q.empty())
+  {
+    // 1. bring out
+    Node *p = q.front();
+    q.pop();
+
+    // 2. all of works
+    int l, r;
+    cin >> l >> r;
+    Node *left;
+    Node *right;
+    if (l == -1)
+      left = NULL;
+    else
+      left = new Node(l);
+    if (r == -1)
+      right = NULL;
+    else
+      right = new Node(r);
+
+    p->left = left;
+    p->right = right;
+
+    // 3. to keep her children
+    if (p->left)
+      q.push(p->left);
+    if (p->right)
+      q.push(p->right);
+  }
+  return root;
+}
+
+int max_height(Node *root)
+{
+  if (root == NULL)
+    return 0;
+
+  int l = max_height(root->left);
+  int r = max_height(root->right);
+  return max(l, r) + 1;
+}
+
+int main()
+{
+  Node *root = input_tree();
+  cout << max_height(root) << endl;
+
+  return 0;
+}

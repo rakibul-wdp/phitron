34b5f1e295e2fb4aa6ec1548c09e4d7a23074d86
rakibul-wdp <rakibul.wdp@gmail.com>
2024-03-23 05:39:00 +0600

    search in bst

---
+++ basic-data-structures/module-21_binary-search-tree/21-3_search-in-BST/search_in_bst.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  Node *left;
+  Node *right;
+  Node(int val)
+  {
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+Node *input_tree()
+{
+  int val;
+  cin >> val;
+  Node *root;
+  if (val == -1)
+    root = NULL;
+  else
+    root = new Node(val);
+
+  queue<Node *> q;
+  if (root)
+    q.push(root);
+
+  while (!q.empty())
+  {
+    // 1. bring out
+    Node *p = q.front();
+    q.pop();
+
+    // 2. all of works
+    int l, r;
+    cin >> l >> r;
+    Node *myLeft;
+    Node *myRight;
+    if (l == -1)
+      myLeft = NULL;
+    else
+      myLeft = new Node(l);
+    if (r == -1)
+      myRight = NULL;
+    else
+      myRight = new Node(r);
+
+    p->left = myLeft;
+    p->right = myRight;
+
+    // 3. to keep her children
+    if (p->left)
+      q.push(p->left);
+    if (p->right)
+      q.push(p->right);
+  }
+  return root;
+}
+
+void level_order(Node *root)
+{
+  if (root == NULL)
+    return;
+
+  queue<Node *> q;
+  q.push(root);
+
+  while (!q.empty())
+  {
+    // 1. bring out
+    Node *f = q.front();
+    q.pop();
+
+    // all of works
+    cout << f->val << " ";
+
+    // 3. to keep her children
+    if (f->left)
+      q.push(f->left);
+    if (f->right)
+      q.push(f->right);
+  }
+}
+
+bool search(Node *root, int x)
+{
+  if (root == NULL)
+    return false;
+
+  if (root->val == x)
+    return true;
+
+  if (x < root->val)
+    return search(root->left, x);
+  else
+    return search(root->right, x);
+}
+
+int main()
+{
+  Node *root = input_tree();
+
+  if (search(root, 77))
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  return 0;
+}

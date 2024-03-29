f5a2dce92f865d2d744cf515869b0a3d23efb8c7
rakibul-wdp <rakibul.wdp@gmail.com>
2024-03-30 03:39:00 +0600

    adjacency list implementation

---
+++ introduction-to-algorithms/module-01_basics-of-graph-and-representations/1-6_adjacency-list-implementation/adjacency_list.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n, e;
+  cin >> n >> e;
+
+  vector<int> mat[n];
+  while (e--)
+  {
+    int a, b;
+    cin >> a >> b;
+    mat[a].push_back(b);
+    mat[b].push_back(a);
+  }
+
+  vector<int> v;
+  for (int i = 0; i < mat[3].size(); i++)
+    cout << mat[3][i] << " ";
+
+  return 0;
+}
+
+/*
+input:
+6 6
+0 1
+1 5
+0 4
+0 3
+3 4
+2 4
+*/

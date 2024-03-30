7ba77783ec84350f0c41dbfac68f9742d6b371cb
rakibul-wdp <rakibul.wdp@gmail.com>
2024-03-31 04:39:00 +0600

    again list pair

---
+++ introduction-to-algorithms/module-01_basics-of-graph-and-representations/1-7_stl-pair/again_stl_pair.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  pair<string, int> p;
+  p = make_pair("abul", 20);
+  cout << p.first << " " << p.second << endl;
+
+  return 0;
+}

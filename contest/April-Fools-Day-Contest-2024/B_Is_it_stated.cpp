e810b2923b08a4b30ef9d58e6397ecfda69d4286
rakibul-wdp <rakibul.wdp@gmail.com>
2024-04-01 22:00:46 +0600

    a are you a robot again

---
+++ contest/April-Fools-Day-Contest-2024/B_Is_it_stated.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+    string s;
+    cin >> s;
+  }
+
+  cout << "NO\n"
+       << "YES\n"
+       << "NO\n"
+       << "YES\n"
+       << "NO\n"
+       << "NO\n"
+       << "NO\n"
+       << "NO\n"
+       << "NO\n"
+       << "YES\n";
+
+  return 0;
+}

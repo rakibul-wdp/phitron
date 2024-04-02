4b162468ae76bfaed39697305a1946e46dcb81bf
rakibul-wdp <rakibul.wdp@gmail.com>
2024-04-03 02:39:00 +0600

    adjacency matrix implementation

---
+++ introduction-to-algorithms/module-01_basics-of-graph-and-representations/1-4_adjacency-matrix-implementation/am.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n, e;
+  cin >> n >> e;
+  int mat[n][n];
+  memset(mat, 0, sizeof(mat));
+  while (e--)
+  {
+    int a, b;
+    cin >> a >> b;
+    mat[a][b] = 1;
+    mat[b][a] = 1;
+  }
+
+  if (mat[3][5] == 1)
+    cout << "connected";
+  else
+    cout << "not connected";
+
+  return 0;
+}
+
+/*
+6 6
+0 1
+1 5
+0 4
+0 3
+3 4
+2 4
+*/

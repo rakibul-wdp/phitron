a6273c12fd85a74ab26c2562578ab1e0fa6b3448
rakibul-wdp <rakibul.wdp@gmail.com>
2024-04-04 02:39:00 +0600

    triple again stl pair

--- introduction-to-algorithms/module-01_basics-of-graph-and-representations/1-7_stl-pair/stlp.cpp
+++ introduction-to-algorithms/module-01_basics-of-graph-and-representations/1-7_stl-pair/stlp.cpp
@@ -1,21 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Pair
-{
-public:
-  int first, second;
-  void make_pair(int a, int b)
-  {
-    first = a;
-    second = b;
-  }
-};
-
 int main()
 {
-  Pair p;
-  p.make_pair(10, 20);
+  pair<string, int> p;
+  p = make_pair("abul", 20);
 
   cout << p.first << " " << p.second << endl;
 

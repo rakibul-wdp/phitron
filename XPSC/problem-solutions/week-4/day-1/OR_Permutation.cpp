699c7af86f6b94c271c646d0a13cb3f6469aca41
rakibul-wdp <rakibul.wdp@gmail.com>
2024-04-05 22:04:15 +0600

    OR Permutation

---
+++ XPSC/problem-solutions/week-4/day-1/OR_Permutation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int testCases;
+  cin >> testCases;
+
+  while (testCases--)
+  {
+    int n;
+    cin >> n;
+
+    for (int i = n; i > 0; i--)
+      cout << i << " ";
+
+    cout << "\n";
+  }
+}

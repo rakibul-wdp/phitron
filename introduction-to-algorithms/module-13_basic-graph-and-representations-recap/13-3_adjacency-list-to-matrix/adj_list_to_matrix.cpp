63b8817df5835cad1347fe5eb9d88f02f80b8e22
rakibul-wdp <rakibul.wdp@gmail.com>
2024-04-11 02:39:00 +0600

    adjacency list to matrix second

--- introduction-to-algorithms/module-13_basic-graph-and-representations-recap/13-3_adjacency-list-to-matrix/adj_list_to_matrix.cpp
+++ introduction-to-algorithms/module-13_basic-graph-and-representations-recap/13-3_adjacency-list-to-matrix/adj_list_to_matrix.cpp
@@ -9,6 +9,13 @@ void convert(int n, vector<int> adj[])
   {
     for (int child : adj[i])
       mat[i][child] = 1;
+
+    //   for (int j = 0; j < n; j++)
+    // {
+    //   mat[i][j] = 0;
+    //   if (i == j)
+    //     mat[i][j] = 1;
+    // }
   }
   for (int i = 0; i < n; i++)
   {

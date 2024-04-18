fb5558952d7664a3c46240734568068b612a3454
rakibul-wdp <rakibul.wdp@gmail.com>
2024-04-19 02:39:00 +0600

    fibonacci that run upto 20 within 1 second

--- introduction-to-algorithms/module-17_basic-of-dp/fibonacci.cpp
+++ introduction-to-algorithms/module-17_basic-of-dp/fibonacci.cpp
@@ -1,29 +1,20 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-const ll N = 1e6 + 5;
-ll dp[N];
-
-ll fibonacci(ll n)
+int fibonacci(int n)
 {
   if (n == 0 || n == 1)
     return n;
 
-  if (dp[n] != -1)
-    return dp[n];
-
-  ll ans = fibonacci(n - 1) + fibonacci(n - 2);
-  dp[n] = ans;
+  int ans = fibonacci(n - 1) + fibonacci(n - 2);
   return ans;
 }
 
 int main()
 {
-  ll n;
+  int n;
   cin >> n;
 
-  memset(dp, -1, sizeof(dp));
   cout << fibonacci(n) << endl;
 
   return 0;

0a413a5a6dd8e5a1d00ee2c69a15ca2f0ce0c52f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-04-19 03:39:00 +0600

    memoization on fibonacci series

--- introduction-to-algorithms/module-17_basic-of-dp/fibonacci.cpp
+++ introduction-to-algorithms/module-17_basic-of-dp/fibonacci.cpp
@@ -1,20 +1,29 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-int fibonacci(int n)
+const ll N = 1e6 + 5;
+ll dp[N];
+
+ll fibonacci(ll n)
 {
   if (n == 0 || n == 1)
     return n;
 
-  int ans = fibonacci(n - 1) + fibonacci(n - 2);
+  if (dp[n] != -1)
+    return dp[n];
+
+  ll ans = fibonacci(n - 1) + fibonacci(n - 2);
+  dp[n] = ans;
   return ans;
 }
 
 int main()
 {
-  int n;
+  ll n;
   cin >> n;
 
+  memset(dp, -1, sizeof(dp));
   cout << fibonacci(n) << endl;
 
   return 0;

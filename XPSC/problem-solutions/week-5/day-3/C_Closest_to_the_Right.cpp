8d622490130cbcb3cc718bac41f151a16f756342
rakibul-wdp <rakibul.wdp@gmail.com>
2024-04-21 09:45:56 +0600

    C Closest to the Right

---
+++ XPSC/problem-solutions/week-5/day-3/C_Closest_to_the_Right.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n, k;
+  cin >> n >> k;
+
+  int arr[n];
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  while (k--)
+  {
+    int key;
+    cin >> key;
+
+    int l = 0, r = n - 1, mid, ans = n;
+
+    while (l <= r)
+    {
+      mid = (l + r) / 2;
+      if (key <= arr[mid])
+      {
+        ans = mid;
+        r = mid - 1;
+      }
+      else
+        l = mid + 1;
+    }
+
+    cout << ans + 1 << "\n";
+  }
+
+  return 0;
+}

f4a2acc7e41ea036cf9bf04ae2307aadd79ed411
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-05 06:05:21 +0600

    C Replacement

--- basic-data-structures/module-2.5_week-1-practice-1/C_Replacement.cpp
+++ basic-data-structures/module-2.5_week-1-practice-1/C_Replacement.cpp
@@ -12,9 +12,14 @@ int main()
 
   for (int i = 0; i < n; i++)
   {
-    if (v[i] >= 0)
-      replace(v.begin(), v.end())
+    if (v[i] > 0)
+      replace(v.begin(), v.end(), v[i], 1);
+    else if (v[i] < 0)
+      replace(v.begin(), v.end(), v[i], 2);
   }
+
+  for (int x : v)
+    cout << x << " ";
 
   return 0;
 }

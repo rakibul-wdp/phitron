e9426ac9c6618f8354d46ed36301901b70a2775c
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-08 01:39:00 +0600

    A Right There

---
+++ contest/Practice-Contest-2-Div-4/A_Right_There.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+    int n, x;
+    cin >> n >> x;
+
+    if (n <= x)
+      cout << "YES"
+           << "\n";
+    else
+      cout << "NO"
+           << "\n";
+  }
+
+  return 0;
+}

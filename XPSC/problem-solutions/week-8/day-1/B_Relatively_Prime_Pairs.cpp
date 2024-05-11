a663ae2e36b319c11fff534e53de3773d6c920ed
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-11 20:04:06 +0600

    B Relatively Prime Pairs

---
+++ XPSC/problem-solutions/week-8/day-1/B_Relatively_Prime_Pairs.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  long long l, r;
+  cin >> l >> r;
+
+  cout << "YES\n";
+
+  for (long long i = l; i < r + 1; i += 2)
+    cout << i << " " << i + 1 << "\n";
+
+  return 0;
+}

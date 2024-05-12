398e98dd13bd26d079d4946b583d1172cc19999a
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-13 03:39:00 +0600

    again count letters

--- basic-data-structures/module-2.5_week-1-practice-1/J_Count_Letters.cpp
+++ basic-data-structures/module-2.5_week-1-practice-1/J_Count_Letters.cpp
@@ -6,5 +6,17 @@ int main()
   string s;
   cin >> s;
 
+  int count[26] = {0};
+
+  for (int i = 0; i < s.length(); i++)
+  {
+    int value = s[i] - 'a';
+    count[value]++;
+  }
+
+  for (int i = 0; i < 26; i++)
+    if (count[i] != 0)
+      cout << char(i + 'a') << " : " << count[i] << "\n";
+
   return 0;
 }

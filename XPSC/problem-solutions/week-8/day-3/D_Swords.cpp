eaef5b3f05ca37c82c12b8c1ba37e1e04153679c
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-13 08:45:42 +0600

    D Swords

---
+++ XPSC/problem-solutions/week-8/day-3/D_Swords.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  int mx = 0;
+  vector<int> v(n);
+
+  for (auto &x : v)
+  {
+    cin >> x;
+    mx = max(mx, x);
+  }
+
+  int gcd = 0;
+  long long sum = 0;
+
+  for (auto x : v)
+  {
+    gcd = __gcd(gcd, mx - x);
+    sum += (mx - x);
+  }
+
+  long long ans = (gcd == 0 ? 0 : sum / gcd);
+  cout << ans << " " << gcd << "\n";
+
+  return 0;
+}

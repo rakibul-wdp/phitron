72654bb31eaaa1b2f49bf11b39e289e130b00659
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-14 01:39:00 +0600

    again d counting elements

--- basic-data-structures/module-2.5_week-1-practice-1/D_Counting_Elements.cpp
+++ basic-data-structures/module-2.5_week-1-practice-1/D_Counting_Elements.cpp
@@ -5,19 +5,20 @@ int main()
 {
   int n;
   cin >> n;
+
   vector<int> v(n);
   for (int i = 0; i < n; i++)
     cin >> v[i];
 
   int count = 0;
-  for (int val : v)
+  for (int i = 0; i < n; i++)
   {
-    auto it = find(v.begin(), v.end(), val + 1);
+    auto it = find(v.begin(), v.end(), v[i] + 1);
     if (it != v.end())
       count++;
   }
 
-  cout << count << endl;
+  cout << count << "\n";
 
   return 0;
 }

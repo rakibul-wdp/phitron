004bbeb117333886f35c0cba2b61115a2c8612df
rakibul-wdp <rakibul.wdp@gmail.com>
2024-06-18 03:39:00 +0600

    F Reversing re-solve

--- basic-data-structures/module-2.5_week-1-practice-1/F_Reversing.cpp
+++ basic-data-structures/module-2.5_week-1-practice-1/F_Reversing.cpp
@@ -10,10 +10,8 @@ int main()
   for (int i = 0; i < n; i++)
     cin >> v[i];
 
-  reverse(v.begin(), v.end());
-
-  for (int x : v)
-    cout << x << " ";
+  for (int i = n - 1; i >= 0; i--)
+    cout << v[i] << " ";
 
   return 0;
 }

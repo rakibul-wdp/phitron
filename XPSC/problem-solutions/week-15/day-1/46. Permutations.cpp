e50fc511877d29f6c168ef16fb40605dfddc65e3
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-07 04:35:20 +0600

    46 Permutations

---
+++ XPSC/problem-solutions/week-15/day-1/46. Permutations.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+  vector<vector<int>> permute(vector<int> &nums)
+  {
+    vector<vector<int>> res;
+    if (nums.size() == 1)
+    {
+      vector<int> singleList;
+      singleList.push_back(nums[0]);
+      res.push_back(singleList);
+      return res;
+    }
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      int n = nums[i];
+      vector<int> remainingNums;
+      for (int j = 0; j < nums.size(); j++)
+      {
+        if (j != i)
+        {
+          remainingNums.push_back(nums[j]);
+        }
+      }
+
+      vector<vector<int>> perms = permute(remainingNums);
+      for (vector<int> p : perms)
+      {
+        p.insert(p.begin(), n);
+        res.push_back(p);
+      }
+    }
+
+    return res;
+  }
+};
